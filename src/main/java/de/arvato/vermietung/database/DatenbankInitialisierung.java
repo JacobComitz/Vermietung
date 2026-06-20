d25752e30a07666d5e715acc1976f5519e4bda97
COMI008 <jacob.comitz@bertelsmann.de>
2026-06-20 12:45:30 +0200

    DatenbankInitialisierung: Kunde,Produkt,Mietung

--- src/main/java/de/arvato/vermietung/database/DatenbankInitialisierung.java
+++ src/main/java/de/arvato/vermietung/database/DatenbankInitialisierung.java
@@ -5,19 +5,49 @@ import java.sql.Statement;
 
 public class DatenbankInitialisierung {
 
-    public static void initialisierung(){
+    public static void initialisierung() {
 
-        String sql = """
-                    CREATE  TABLE IF NOT EXISTS kunden (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        name TEXT NOT NULL,
-                        email TEXT
-                );
-                """;
+        String sqlKunde =
+                """
+                        CREATE  TABLE IF NOT EXISTS kunden (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                email TEXT
+                        );
+                        """;
+
+
+        String sqlProdukt =
+                """
+                        
+                        CREATE  TABLE IF NOT EXISTS produkte (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                beschreibung TEXT,
+                                kategorie TEXT,
+                                anzahl INTEGER,
+                                preisProTag DOUBLE,
+                                bildpfad TEXT
+                        );
+                        """;
+
+        String sqlMietung =
+                """
+                        
+                        CREATE  TABLE IF NOT EXISTS mietung (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                produktID TEXT,
+                                startdatum TEXT,
+                                mietdauer INTEGER
+                        );
+                        """;
 
         try (Connection con = DatenbankConnection.verbinden();
              Statement stmt = con.createStatement()) {
-            stmt.execute(sql);
+            stmt.execute(sqlProdukt);
+            stmt.execute(sqlKunde);
+            stmt.execute(sqlMietung);
             System.out.println("Tabellen geprüft");
 
         } catch (Exception e) {
