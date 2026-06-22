0a99b63a1d45d6802a69b4e1ed09ad28fcc61826
COMI008 <jacob.comitz@bertelsmann.de>
2026-06-22 09:58:44 +0200

    UserRepository mit pstmt.executeUpdate

--- src/main/java/de/arvato/vermietung/repository/UserRepository.java
+++ src/main/java/de/arvato/vermietung/repository/UserRepository.java
@@ -1,1 +1,24 @@
-// hier weitermachen
+package de.arvato.vermietung.repository;
+import de.arvato.vermietung.database.DatenbankConnection;
+import de.arvato.vermietung.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserRepository {
+    public static void speichern(User user){
+        try(Connection con = DatenbankConnection.verbinden()){
+            String sql = "INSERT INTO kunden (name,email) VALUES (TEXT,TEXT)";
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2,user.getEmail());
+
+            pstmt.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
