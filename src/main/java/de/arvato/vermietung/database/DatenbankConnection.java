f78ac5317ec71afa7cde3751930435386a548bbf
COMI008 <jacob.comitz@bertelsmann.de>
2026-06-25 07:39:08 +0200

    DatenbankConnection neu gemacht - Test

--- src/main/java/de/arvato/vermietung/database/DatenbankConnection.java
+++ src/main/java/de/arvato/vermietung/database/DatenbankConnection.java
@@ -5,7 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatenbankConnection {
-    public static Connection verbinden() throws SQLException {
-        return DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=AngelVermietung;integratedSecurity=true;trustServerCertificate=true;");
+    private Connection DbConnection;
+
+    public void DbConnection(){
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url ="jdbc:sqlserver://localhost:1433;databaseName=test;encrypt=true;trustServerCertificate=true;";
+            String user ="AngelVermietung";
+            String password ="Angelvermietung";
+            this.DbConnection = DriverManager.getConnection(url,user,password);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
