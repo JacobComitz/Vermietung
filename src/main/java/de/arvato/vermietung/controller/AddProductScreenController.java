ce3553b159be0b1ea108dcc1736574d1fa046eff
COMI008 <jacob.comitz@bertelsmann.de>
2026-06-26 09:41:26 +0200

    AddProductScreenController - speichern() erstellt

--- src/main/java/de/arvato/vermietung/controller/AddProductScreenController.java
+++ src/main/java/de/arvato/vermietung/controller/AddProductScreenController.java
@@ -1,8 +1,45 @@
 package de.arvato.vermietung.controller;
 
+import de.arvato.vermietung.model.Produkt;
+import de.arvato.vermietung.repository.ProduktRepository;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.lang.classfile.constantpool.DoubleEntry;
 
 public class AddProductScreenController {
+
+    @FXML private TextField TextfieldName;
+    @FXML private TextField TextfieldBeschreibung;
+    @FXML private TextField TextfieldKategorie;
+    @FXML private TextField TextfieldAnzahl;
+    @FXML private TextField TextfieldPreisProTag;
+    @FXML private TextField TextfieldBildpfad;
+    @FXML private Button buttonSpeichern;
+
+    public String getName(){
+        return TextfieldName.getText();
+    }
+    public String getBeschreibung(){
+        return TextfieldBeschreibung.getText();
+    }
+    public String getKategorie(){
+        return TextfieldKategorie.getText();
+    }
+    public int getAnzahl(){
+        return Integer.parseInt(TextfieldAnzahl.getText());
+    }
+    public double getPreisProTag(){
+        return Double.parseDouble(TextfieldPreisProTag.getText());
+    }
+    public String getBildpfad(){
+        return TextfieldBildpfad.getText();
+    }
+
     public void speichern(ActionEvent event) {
+        Produkt produkt = new Produkt(getName(),getBeschreibung(),getKategorie(),getAnzahl(),getPreisProTag(),getBildpfad());
+        ProduktRepository.speichern(produkt);
     }
 }
