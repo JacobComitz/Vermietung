d0ada24c60e0529c66f256fdcbf698f60bd2530a
COMI008 <jacob.comitz@bertelsmann.de>
2026-07-14 14:56:42 +0200

    UserProductScreenController - Initialize, showall, zeigeprodukte

--- src/main/java/de/arvato/vermietung/controller/UserProductScreenController.java
+++ src/main/java/de/arvato/vermietung/controller/UserProductScreenController.java
@@ -1,18 +1,27 @@
 package de.arvato.vermietung.controller;
 
+import de.arvato.vermietung.model.Produkt;
+import de.arvato.vermietung.repository.ProduktRepository;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class UserProductScreenController {
     private Scene scene;
     private Scene stage;
     private Scene Parent;
+    private TilePane productTilePane;
 
     public void openCart(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/de/arvato/vermietung/Checkout/cart.fxml"));
@@ -22,7 +31,13 @@ public class UserProductScreenController {
         stage.show();
     }
 
+    @FXML
+    public void initialize(){
+        zeigeProdukte(ProduktRepository.findeAlle());
+    }
+
     public void showAll(ActionEvent event) {
+        zeigeProdukte(ProduktRepository.findeAlle());
     }
 
     public void showRuten(ActionEvent event) {
@@ -52,4 +67,19 @@ public class UserProductScreenController {
         stage.setScene(scene);
         stage.show();
     }
+
+    private void zeigeProdukte(List<Produkt> produkte ){
+        productTilePane.getChildren().clear();
+        for(Produkt produkt : produkte){
+            VBox karte = new VBox(10);
+            karte.setAlignment(Pos.CENTER);
+            karte.getStyleClass().add("product-card");
+            Label name = new Label(produkt.getName());
+            Label preis = new Label(produkt.getPreisProTag() + "€ / Tag");
+            karte.getChildren().addAll(name,preis);
+            productTilePane.getChildren().add(karte);
+        }
+    }
+
+
 }
